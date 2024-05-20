c69a53ac614e9093d0032a3ae66228cc50be6f9a
Ted Herambert <tedherambert2004@gmail.com>
2024-05-20 17:27:35 +0200

    ajouterRessource

--- Pioche.java
+++ Pioche.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pioche
-{
+{	
 	private List<Jeton> jetons;
 
 	public Pioche()
@@ -21,21 +21,21 @@ public class Pioche
 
 	private void initPioche()
 	{
-		this.jetons.add(new Jeton(Epice.CANNELLE ));
-		this.jetons.add(new Jeton(Epice.SAFRAN   ));
-		this.jetons.add(new Jeton(Epice.SAFRAN   ));
-		this.jetons.add(new Jeton(Epice.SAFRAN   ));
-		this.jetons.add(new Jeton(Epice.PAPRIKA  ));
-		this.jetons.add(new Jeton(Piece.ARGENT   ));
-		this.jetons.add(new Jeton(Piece.ARGENT   ));
 		this.jetons.add(new Jeton(Epice.POIVRE   ));
+		this.jetons.add(new Jeton(Epice.SUMAC    ));
+		this.jetons.add(new Jeton(Epice.CURCUMA  ));
+		this.jetons.add(new Jeton(Epice.CARDAMONE));
+		this.jetons.add(new Jeton(Piece.OR       ));
+		this.jetons.add(new Jeton(Epice.SAFRAN   ));
+		this.jetons.add(new Jeton(Epice.SESAME   ));
+		this.jetons.add(new Jeton(Epice.POIVRE   ));
+		this.jetons.add(new Jeton(Piece.ARGENT   ));
+		this.jetons.add(new Jeton(Piece.ARGENT   ));
+		this.jetons.add(new Jeton(Epice.PAPRIKA  ));
 		this.jetons.add(new Jeton(Epice.SESAME   ));
 		this.jetons.add(new Jeton(Epice.SAFRAN   ));
-		this.jetons.add(new Jeton(Piece.OR       ));
-		this.jetons.add(new Jeton(Epice.CARDAMONE));
-		this.jetons.add(new Jeton(Epice.CURCUMA  ));
-		this.jetons.add(new Jeton(Epice.SUMAC    ));
-		this.jetons.add(new Jeton(Epice.POIVRE   ));
-
+		this.jetons.add(new Jeton(Epice.SAFRAN   ));
+		this.jetons.add(new Jeton(Epice.SAFRAN   ));
+		this.jetons.add(new Jeton(Epice.CANNELLE ));
 	}
 }
--- Plateau.java
+++ Plateau.java
@@ -1,52 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Plateau
 {
 	private final static int NB_PIECE_MAX = 8;
 	private int nbPiece;
 
+	private int score;
+	private String detailScore;
 
-	private Epice[][] pieces;
+	private List<List<Epice>> tabEpice;
+	private final static int NB_COL_TAB_EPICE = 5;
+	private final static int NB_LIG_TAB_EPICE = 3;
+	private int colTabEpice;
 
 	public Plateau()
 	{
 		this.nbPiece = 0;
-		this.pieces  = new Epice[3][5];
+		this.tabEpice = new ArrayList<List<Epice>>();
+		for(int i = 0; i < Plateau.NB_COL_TAB_EPICE; i++)
+			this.tabEpice.add(new ArrayList<Epice>());
+		this.colTabEpice = 0;
 	}
+
+	public int getScore() { return this.score; }
+	public String getDetailScore() { return this.detailScore; }
 
 	public boolean ajouterRessource(Jeton r)
 	{
 		boolean retour = false;
-		// SI epice
-			// SI meme type que colonne actu
-			// FAIRE placer dans col actu --> TRUE
-			// SINON SI nouvelle col existe ET NON col epice existe
-			// FAIRE placer dans col suivante --> TRUE
-			// SINON --> FALSE
 		
-		// SI piece
-			// FAIRE recuperer valeur en pieces bronzes
-			// SI nb emplacements libres >= nb pieces bronze
-			// FAIRE nbPiece++ --> TRUE
-			// SINON --> FALSE
+		if (r.getType() instanceof Epice)
+		{
+			Epice epice =  (Epice) r.getType();
 
-		return false;
+			boolean boolMemeEpice = false;
+			int i;
+			for(i = 0; i < this.colTabEpice && !boolMemeEpice; i++)
+			{
+				if(this.tabEpice.get(i).size() > 0 &&
+				   this.tabEpice.get(i).get(0).getCouleur().getSymbole().equals(epice.getCouleur().getSymbole())) 
+					boolMemeEpice = true;
+			}
+
+			if(boolMemeEpice)
+			{
+				if (this.tabEpice.get(i-1).size() < Plateau.NB_LIG_TAB_EPICE)
+				{
+					this.tabEpice.get(i-1).add(epice);
+					retour = true;
+				}
+			}
+			else
+			{
+				if(this.colTabEpice < Plateau.NB_COL_TAB_EPICE)
+				{
+					this.tabEpice.get(this.colTabEpice++).add(epice);
+					retour = true;
+				}
+			}
+		}
+
+		if (r.getType() instanceof Piece)
+		{
+			Piece piece =  (Piece) r.getType();
+
+			if (this.nbPiece + piece.getValeur() <= Plateau.NB_PIECE_MAX)
+			{
+				this.nbPiece += piece.getValeur();
+				retour = true;
+			}
+		}
+
+		return retour;
+	}
+
+	public void score()
+	{
+		
 	}
 
 	public String toString()
 	{
 		String retour = "";
-
 		String ligne  = "+-----+-----+-----+-----+-----+";
+		String tmp = "";
 		
 
-		for(int i = 0; i < this.pieces.length; i++)
+		for(int i = 0; i < Plateau.NB_LIG_TAB_EPICE; i++)
 		{
-			retour += ligne + "\n|";
-			for(int j = 0; j < this.pieces[0].length; j++)
+			tmp = ligne + "\n|";
+			for(int j = 0; j < Plateau.NB_COL_TAB_EPICE; j++)
 			{
-				if (this.pieces[i][j] == null) retour += "     |";
-				else                           retour += " " + String.format("%3s", this.pieces[i][j].getLibCourt()) + " |";
+				if (this.tabEpice.get(j).size() <= i) 
+					tmp += "     |";
+				else 
+					tmp += " " + String.format("%3s", this.tabEpice.get(j).get(i).getLibCourt()) + " |";
 			}
-			retour += "\n";
+			retour = tmp + "\n" + retour;
 		}
 		retour += ligne + "\n";
 
